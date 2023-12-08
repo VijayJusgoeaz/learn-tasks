cf5ff22fb34b902d59776207f69b92ae52b16c91
Vijay Ravichandran <vijay@jusgoeaz.com>
2023-12-08 12:12:59 +0530

    enhancement

--- src/Comp/List.tsx
+++ src/Comp/List.tsx
@@ -1,7 +1,15 @@
 import React, { memo } from "react";
 
-const List = memo(function List({ render }: { render: boolean }) {
-  console.log('Render called')
+const List = ({ render }: { render: boolean }) => {
+  console.log("Render called");
+  // making this UI render an expensive only manually.
+  let i = 0;
+  while (i < 2000000000) {
+    i++;
+  }
   return <span>{render.toString()}</span>;
-});
-export default List;
+};
+
+// since we are using memo, this will be rendered only once unless the props[render] value changes
+const MemoList = memo(List);
+export default MemoList;
