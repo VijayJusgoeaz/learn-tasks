6a6fe63e33c64e7920232c597da9d868ec11885e
Vijay Ravichandran <vijay@jusgoeaz.com>
2023-12-11 21:43:00 +0530

    use State Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
-import React, { useMemo, useState } from "react";
-import List from "./Comp/List";
+import React, { useState } from "react";
 
 const App = () => {
-  const [counterOne, setcounterOne] = useState(0);
-  const [counterTwo, setcounterTwo] = useState(0);
+  const [age, setAge] = useState(2);
+  const [age2, setAge2] = useState(2);
 
-  const incrementCounterOne = () => {
-    setcounterOne(counterOne + 2);
-  };
-  const incrementCounterTwo = () => {
-    setcounterTwo(counterTwo + 1);
-  };
-
-  const isEven = useMemo(() => {
-    console.log("Im running....");
-    let i = 0;
-    while (i < 2000000000) {
-      i++;
-    }
-    return counterOne % 2 == 0;
-  }, [counterOne]);
-
-  // const render = useMemo(() => {
-  //   console.log('Render called')
-  //   return isEven;
-  // }, [isEven]);
-
+  function handleClick() {
+    const temp = age * 2;
+    setAge(temp);
+    setAge2(temp / 2);
+  }
   return (
     <div>
-      <button onClick={incrementCounterOne}>Counter One: {counterOne}</button>
-      {/* prevent re-rendering using useMemo */}
-      {/* <span>{render.toString()}</span>; */} 
-
-      {/* prevent re-rendering using memo */}
-      <List render={isEven}></List>
-      <br></br>
-      <button onClick={incrementCounterTwo}>Counter Two: {counterTwo}</button>
+      <div>{age}</div>
+      <div>{age2}</div>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 };
